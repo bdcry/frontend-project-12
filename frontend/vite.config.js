5e6535ee32ba460323585af2fc902a123556b180
Tim <tim@MacBook-Pro-2.local>
2024-12-05 02:25:09 +0300

    Step 3 | Try to deploy project on Render 3

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5001',
-        changeOrigin: true,
-      },
-    },
-  },
-  build: {
-    rollupOptions: {
-      external: ['i18next'],
-    },
-  },
-});
+        target: 'http://localhost:5001'
+      }
+    }
+  }
+})
