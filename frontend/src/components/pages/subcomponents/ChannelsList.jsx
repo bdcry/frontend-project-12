2f42a7e4e181bed618e4fc43144bcb821ea82bcf
Tim <tim@MacBook-Pro-2.local>
2024-12-10 04:12:17 +0300

    Step 6 | Fix layouts design

--- frontend/src/components/pages/subcomponents/ChannelsList.jsx
+++ frontend/src/components/pages/subcomponents/ChannelsList.jsx
@@ -9,23 +9,34 @@ const ChannelsList = ({ data }) => {
 
   return channels.map((channel) => (
     <Nav.Item as="li" className="w-100" key={channel.id}>
-      <Button
-        type="button"
-        className="w-100 rounded-0 text-start text-truncate"
-        variant={channel.id === activeChannelId ? 'secondary' : ''}
-        onClick={() => dispatch(selectActiveTab(channel.id))}
-      >
-        <span className="me-1">#</span>
-        {channel.name}
-      </Button>
+      {!channel.removable && (
+        <Button
+            type="button"
+            className="w-100 rounded-0 text-start text-truncate"
+            variant={channel.id === activeChannelId ? 'secondary' : ''}
+            onClick={() => dispatch(selectActiveTab(channel.id))}
+          >
+            <span className="me-1">#</span>
+            {channel.name}
+          </Button>
+      )}
       {channel.removable && (
         <Dropdown className="d-flex btn-group" role="group">
+          <Button
+            type="button"
+            className="w-100 rounded-0 text-start text-truncate"
+            variant={channel.id === activeChannelId ? 'secondary' : ''}
+            onClick={() => dispatch(selectActiveTab(channel.id))}
+          >
+            <span className="me-1">#</span>
+            {channel.name}
+          </Button>
           <Dropdown.Toggle
             variant={channel.id === activeChannelId ? 'secondary' : ''}
-            className="flex-grow-0 dropdown-toggle-split"
+            className="flex-grow-0 dropdown-toggle-split rounded-0"
           ></Dropdown.Toggle>
           <Dropdown.Menu>
-            <RemoveChannelModal channelId={channel.id}/>
+            <RemoveChannelModal channelId={channel.id} />
             <Dropdown.Item href="#/action-2">Переименовать</Dropdown.Item>
           </Dropdown.Menu>
         </Dropdown>
