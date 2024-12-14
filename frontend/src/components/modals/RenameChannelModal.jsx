f65a647c31f51a3e20d9c6dc86701c31184d7179
Tim <tim@MacBook-Pro-2.local>
2024-12-14 22:36:35 +0300

    Step 12 | Refactoring: fix linter issue

--- frontend/src/components/modals/RenameChannelModal.jsx
+++ frontend/src/components/modals/RenameChannelModal.jsx
@@ -1,11 +1,11 @@
+import { useFormik } from 'formik';
+import { useTranslation } from 'react-i18next';
+import leoProfanity from 'leo-profanity';
 import { Button, Modal, Form } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import { setStatusChannelModal } from '../../store/slices/modalsSlice';
 import { renameChannelById } from '../../store/slices/channelsSlice';
-import { useFormik } from 'formik';
 import { channelSchema } from '../../utils/validation/validationForm';
-import { useTranslation } from 'react-i18next';
-import leoProfanity from 'leo-profanity';
 
 const RenameChannelModal = () => {
   const dispatch = useDispatch();
@@ -13,25 +13,23 @@ const RenameChannelModal = () => {
   const { activeChannelId } = useSelector(({ ui }) => ui.modals);
   const modalStatus = useSelector(({ ui }) => ui.modals.renameChannelModal);
   const token = useSelector(({ auth }) => auth.token);
-  const channels = useSelector(({ channels }) => channels.channelsData);
-  const channel = channels.find((channel) => channel.id === activeChannelId);
+  const channelsData = useSelector(({ channels }) => channels.channelsData);
+  const currentChannel = channelsData.find((ch) => ch.id === activeChannelId);
 
   const formik = useFormik({
     initialValues: {
-      name: channel.name,
+      name: currentChannel.name,
     },
-    validationSchema: channelSchema(channels, t, channel.name),
+    validationSchema: channelSchema(channelsData, t, currentChannel.name),
     onSubmit: (values, { resetForm }) => {
       const cleanChannelName = leoProfanity.clean(values.name);
       const editedChannel = { name: cleanChannelName };
-      dispatch(renameChannelById({ token, id: channel.id, editedChannel }));
-      dispatch(
-        setStatusChannelModal({
-          modalName: 'renameChannelModal',
-          status: false,
-          channelId: null,
-        })
-      );
+      dispatch(renameChannelById({ token, id: currentChannel.id, editedChannel }));
+      dispatch(setStatusChannelModal({
+        modalName: 'renameChannelModal',
+        status: false,
+        channelId: null,
+      }));
       resetForm();
     },
   });
@@ -39,15 +37,11 @@ const RenameChannelModal = () => {
   return (
     <Modal
       show={modalStatus}
-      onHide={() =>
-        dispatch(
-          setStatusChannelModal({
-            modalName: 'renameChannelModal',
-            status: false,
-            channelId: null,
-          })
-        )
-      }
+      onHide={() => dispatch(setStatusChannelModal({
+        modalName: 'renameChannelModal',
+        status: false,
+        channelId: null,
+      }))}
       dialogClassName="modal-dialog-centered"
     >
       <Modal.Header>
@@ -56,16 +50,12 @@ const RenameChannelModal = () => {
           aria-label="Close"
           data-bs-dismiss="modal"
           variant="close"
-          onClick={() =>
-            dispatch(
-              setStatusChannelModal({
-                modalName: 'renameChannelModal',
-                status: false,
-                channelId: null,
-              })
-            )
-          }
-        ></Button>
+          onClick={() => dispatch(setStatusChannelModal({
+            modalName: 'renameChannelModal',
+            status: false,
+            channelId: null,
+          }))}
+        />
       </Modal.Header>
       <Modal.Body>
         <Form onSubmit={formik.handleSubmit}>
@@ -78,28 +68,28 @@ const RenameChannelModal = () => {
               onChange={formik.handleChange}
               value={formik.values.name}
               isInvalid={formik.errors.name && formik.touched.name}
-            ></Form.Control>
-            <label className="visually-hidden" htmlFor="name">Имя канала</label>
-            <Form.Control.Feedback type="invalid">
-              {formik.errors.name}
-            </Form.Control.Feedback>
+            />
+            <label className="visually-hidden" htmlFor="name">{t('renameChannelModal.label')}</label>
+            {formik.errors.name && (
+              <Form.Control.Feedback type="invalid">
+                {formik.errors.name}
+              </Form.Control.Feedback>
+            )}
             <div className="d-flex justify-content-end">
               <Button
                 variant="secondary"
                 className="me-2"
-                onClick={() =>
-                  dispatch(
-                    setStatusChannelModal({
-                      modalName: 'renameChannelModal',
-                      status: false,
-                      channelId: null,
-                    })
-                  )
-                }
+                onClick={() => dispatch(setStatusChannelModal({
+                  modalName: 'renameChannelModal',
+                  status: false,
+                  channelId: null,
+                }))}
               >
                 {t('renameChannelModal.cancel')}
               </Button>
-              <Button type="submit">{t('renameChannelModal.submit')}</Button>
+              <Button type="submit">
+                {t('renameChannelModal.submit')}
+              </Button>
             </div>
           </Form.Group>
         </Form>
