f3a7754032da73c353b229e973b778220682e2fb
Tim <tim@MacBook-Pro-2.local>
2024-12-20 23:49:36 +0300

    v2 | Refactoring: Add API and navigate routes, move devDependencies, fix toast error on signup page

--- frontend/src/utils/routes.js
+++ frontend/src/utils/routes.js
@@ -4,11 +4,11 @@ import { getTranslator } from './translator';
 const BASE_API_URL = '/api/v1';
 
 export const apiPath = {
-  loginPath: () => [BASE_API_URL, 'login'].join('/'),
-  signupPath: () => [BASE_API_URL, 'signup'].join('/'),
-  messagesPath: () => [BASE_API_URL, 'messages'].join('/'),
-  channelsPath: () => [BASE_API_URL, 'channels'].join('/'),
-  channelPath: (id) => [BASE_API_URL, 'channels', id].join('/'),
+  loginPath: () => `${BASE_API_URL}/login`,
+  signupPath: () => `${BASE_API_URL}/signup`,
+  messagesPath: () => `${BASE_API_URL}/messages`,
+  channelsPath: () => `${BASE_API_URL}/channels`,
+  channelPath: (id) => `${BASE_API_URL}/channels/${id}`,
 };
 
 export const handleApiError = (error) => {
