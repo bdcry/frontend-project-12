c919c5c7ec109a2823a3756a8a5143a14eb6417d
Tim <tim@MacBook-Pro-2.local>
2024-12-22 23:49:15 +0300

    v2.4 | Refactoring: add scroll functionallity for chat and channels

--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -1,4 +1,4 @@
-import { act, useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 import {
